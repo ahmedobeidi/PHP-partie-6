8faceb7e51b539c5ffff99ac15f1d3afaa557736
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-05 14:18:32 +0100

    DONE: partie6-exo1

---
+++ Exercice1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1</title>
+</head>
+<body>
+    <form action="./process/handle-form.php" method="get">
+        <label for="lastname">Last name:</label>
+        <input type="text" name="lastname" id="lastname">
+
+        <label for="firstname">First name:</label>
+        <input type="text" name="firstname" id="firstname">
+
+        <input type="submit" value="submit">   
+    </form>
+</body>
+</html>
---
+++ Exercice1/process/handle-form.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!isset($_GET['lastname'], $_GET['firstname'])) {
+   echo 'No';
+}   
+else {
+    echo 'yes';
+}
