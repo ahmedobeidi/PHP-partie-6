54f159cee10b932ad497582cdcfb6fe15f3dee57
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-05 15:06:16 +0100

    DONE: Exercice 5

---
+++ Exercice5/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 5</title>
+</head>
+<body>
+    <?php 
+        if (isset($_GET['week'])) {
+            var_dump($_GET['week']);
+        }
+    ?>
+</body>
+</html>
